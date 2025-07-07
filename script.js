31c405782f822dde8779b6bdf41d4225aaba12ad
Behzod <noobplayergamer342@gmail.com>
2025-07-07 14:27:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const orderBtn = document.getElementById("orderbtn");
+const orderBtn2 = document.getElementById("orderbtn2");
+const orderForm = document.querySelector(".overlay");
+
+orderBtn.addEventListener("click", () => {
+    orderForm.style.display = 'flex'
+});
+
+orderBtn2.addEventListener("click", () => {
+    orderForm.style.display = 'flex'
+})
