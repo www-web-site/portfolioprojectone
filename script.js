61d57d4a5429f87a217a298aeac9621586c6c816
Behzod <noobplayergamer342@gmail.com>
2025-07-09 22:58:02 +0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const orderBtn = document.getElementById("orderbtn");
 const orderBtn2 = document.getElementById("orderbtn2");
 const orderForm = document.querySelector(".overlay");
+const close = document.querySelector(".close");
+const orderBtn3 = document.querySelector(".thirdOrder")
+
+close.addEventListener("click", () => {
+    orderForm.style.display = 'none'
+});
 
 orderBtn.addEventListener("click", () => {
     orderForm.style.display = 'flex'
@@ -9,3 +15,30 @@ orderBtn.addEventListener("click", () => {
 orderBtn2.addEventListener("click", () => {
     orderForm.style.display = 'flex'
 })
+
+orderBtn3.addEventListener("click", () => {
+    orderForm.style.display = 'flex'
+})
+
+orderForm.addEventListener("click", (e) =>{
+    if(e.target === orderForm){
+        orderForm.style.display = 'none'
+    }
+})
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+AOS.init({
+  duration: 800, 
+  once: true 
+});
